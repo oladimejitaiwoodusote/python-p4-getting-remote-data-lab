ef99dc713d03de8288e90a2d070d61e1e22f89eb
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2023-04-29 14:30:46 -0400

    flask lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,14 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
+        
 
     def load_json(self):
-        pass
+        return json.loads(self.get_response_body())
+        
+
+first = GetRequester("https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json")
+print(first.get_response_body())
+print(first.load_json())
